918d5126bba1da4f0e318addfbb84e9344848e84
Susan Tan <ssusantan9@gmail.com>
2019-09-24 15:56:10 -0400

    fixed routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,32 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { Switch, Route } from 'react-router-dom'
 import Login from './components/Login.js'
 import NewsContainer from './containers/NewsContainer.js'
 import Signup from './components/Signup.js'
 
 
-const Routes = () => {
+class Routes extends React.Component {
+    render(){
+        console.log(this.props.currentUser.username)
     return (
         <Switch>
+            { !this.props.currentUser.username ?
+            <div>
             <Route path='/signup' component={Signup}/>
             <Route path ='/login' component={Login}/>
+            </div>
+                :
             <Route path ='/' component={NewsContainer}/>
+            }
         </Switch>
-    )
+        )
+    }
 }
 
-export default Routes
+const mapStateToProps = state => ({ 
+    currentUser: state.currentUser
+})
+
+
+export default connect(mapStateToProps, null)(Routes)
